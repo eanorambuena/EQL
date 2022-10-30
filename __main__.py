59199021dacacba409c103262632301de36990f3
eanorambuena <eanorambuena@uc.cl>
2022-10-30 18:07:42 -0300

    Improve example and and .gitignore

--- __main__.py
+++ __main__.py
@@ -9,11 +9,19 @@ if __name__ == '__main__':
     ftpstream = urllib.request.urlopen(url)
     csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
     csv_content = ""
+    count = 0
     for line in csvfile:
-        csv_content += ",".join([value.strip() for value in str(line)[1:-1].split(",")]) + "\n"
+        if count == 0:
+            template = [
+                value.strip().replace(" ", "_") for value in str(line)[1:-1].split(",")
+                ]
+        else:
+            csv_content += ",".join([value.strip() for value in str(line)[1:-1].split(",")]) + "\n"
+        count += 1
 
     db = DataBase()
     db.load_from_csv(csv_content)
+    db.template(*template)
         
     while True:
         db.query(input(">>> "))
