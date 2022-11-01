518bab422dfab0f26cc7afec1fce8f33657d53f0
eanorambuena <eanorambuena@uc.cl>
2022-10-31 23:28:23 -0300

    Fix example.py folder creating system and .gitignore

--- example.py
+++ example.py
@@ -1,3 +1,5 @@
+import os
+
 from engine.base import DataBase
 
 if __name__ == '__main__':
@@ -19,7 +21,13 @@ if __name__ == '__main__':
             csv_content += ",".join([value.strip() for value in str(line)[1:-1].split(",")]) + "\n"
         count += 1
 
-    with DataBase("example_data/database.edb") as db:
+    path = ["example_data", "database.edb"]
+
+    folder_path = path[:-1]
+    if not os.path.exists(os.path.join(*folder_path)):
+        os.makedirs(os.path.join(*folder_path))
+
+    with DataBase(os.path.join(*path)) as db:
         db.create_table("medals")
         medals = db.use_table("medals")
         medals.load_from_csv(csv_content)
